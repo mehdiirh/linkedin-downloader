97c4aceb9480a04c90006ee9a6c2dce7fad4bbe8
Mehdi Rahimi <mehdii.rh78@gmail.com>
2022-07-12 12:53:13 +0430

    [UPDATE] Move linkedin account url to config

--- utils/telegram/keyboards.py
+++ utils/telegram/keyboards.py
@@ -1,6 +1,12 @@
 from telethon import Button
 from utils.telegram.tools import Text
 
+from pathlib import Path
+import json
+
+config_path = Path(__file__).resolve().parent.parent.parent / 'config.json'
+linkedin_url = json.load(open(config_path, 'r'))["LINKEDIN"]["URL"]
+
 _i = Button.inline
 _t = Button.text
 _u = Button.url
@@ -17,9 +23,9 @@ SET_LANG_CONVERSATION = [
 class OpenMyLinkedin(Text):
 
     en = [
-        [_u('Open My Linkedin', 'https://linkedin.com/in/mrdownloader/')]
+        [_u('Open My Linkedin', linkedin_url)]
     ]
 
     fa = [
-        [_u('بازکردن لینکدین من', 'https://linkedin.com/in/mrdownloader/')]
+        [_u('بازکردن لینکدین من', linkedin_url)]
     ]
