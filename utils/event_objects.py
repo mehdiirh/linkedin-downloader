7090ee3205fcaa693190fee444d088c4f7322ca3
Mehdi Rahimi <mehdii.rh78@gmail.com>
2022-07-13 17:35:06 +0430

    [ADD] Add classes for badges and invitations

--- utils/event_objects.py
+++ utils/event_objects.py
@@ -1,6 +1,34 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from dataclasses_json import LetterCase, Undefined, dataclass_json
+from linkedin_messaging.api_objects import MiniProfile
+
 from typing import Optional
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL, undefined=Undefined.EXCLUDE)
+@dataclass
+class Badge:
+    count: int = ""
+    tab: str = ""
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL, undefined=Undefined.EXCLUDE)
+@dataclass
+class TabBadges:
+    tab_badges: list[Badge] = field(default_factory=list)
+
+
+@dataclass_json(letter_case=LetterCase.CAMEL, undefined=Undefined.EXCLUDE)
+@dataclass
+class Invite:
+    sent_time: int = -1
+    invitation_type: str = ""
+    shared_secret: str = ""
+    entity_urn: str = ""
+    to_member_id: str = ""
+    unseen: bool = False
+    from_member: Optional[MiniProfile] = None
+    to_member: Optional[MiniProfile] = None
 
 
 @dataclass_json(letter_case=LetterCase.SNAKE, undefined=Undefined.EXCLUDE)
@@ -25,4 +53,3 @@ class EventDetails:
     sender: str = ""
     author: str = ""
     media: Optional[Media] = None
-
